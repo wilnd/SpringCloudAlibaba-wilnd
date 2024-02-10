9370d0824b4c9fb0f55d9a955fbc3d99a2871782
winld <287329409@qq.com>
2024-02-10 20:09:47 +0800

    快速排序

---
+++ spring-boot-mybatisplus/src/main/java/com/ch/study/algorithm/QuickSort.java
@@ -0,0 +1,40 @@
+package com.ch.study.algorithm;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] array = new int[]{2, 34, 1, 2, 4, 6, 21};
+        quickSort(array, 0, array.length - 1);
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+    public static void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(arr, left, right);
+            quickSort(arr, left, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, right);
+        }
+    }
+
+    public static int partition(int[] arr, int left, int right) {
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (arr[i] < arr[pivot]) {
+                swap(arr, i, index);
+                index++;
+            }
+        }
+        swap(arr, pivot, index - 1);
+        return index - 1;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+}
