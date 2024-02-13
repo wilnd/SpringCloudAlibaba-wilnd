cddcf7ec95ec26d322154abb497c30eb39f210f9
winld <287329409@qq.com>
2024-02-13 19:11:35 +0800

    搜索插入排序

---
+++ spring-boot-mybatisplus/src/main/java/com/ch/study/leecode/SearchInsertPosition.java
@@ -0,0 +1,25 @@
+package com.ch.study.leecode;
+
+/**
+ * 搜索插入位置
+ * 二分查找
+ */
+public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+}
