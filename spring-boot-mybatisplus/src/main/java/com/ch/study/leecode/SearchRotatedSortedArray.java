82d5dca8a9aa17a5b82d9d543cf48d99a027b539
winld <287329409@qq.com>
2024-02-13 16:52:13 +0800

    下一个排列

---
+++ spring-boot-mybatisplus/src/main/java/com/ch/study/leecode/SearchRotatedSortedArray.java
@@ -0,0 +1,34 @@
+package com.ch.study.leecode;
+
+/**
+ * 搜索旋转排序数组
+ * 二分查找
+ */
+public class SearchRotatedSortedArray {
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < nums[right]) {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (target < nums[mid] && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+}
