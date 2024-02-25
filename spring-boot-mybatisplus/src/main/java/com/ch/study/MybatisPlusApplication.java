8026de9e4f5c598889996b082f680b380c5232a1
winld <287329409@qq.com>
2024-02-25 17:03:11 +0800

    sentinel 注解模式

--- spring-boot-mybatisplus/src/main/java/com/ch/study/MybatisPlusApplication.java
+++ spring-boot-mybatisplus/src/main/java/com/ch/study/MybatisPlusApplication.java
@@ -1,13 +1,23 @@
 package com.ch.study;
 
+import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("com.ch.study.mapper")
+@EnableAspectJAutoProxy
 public class MybatisPlusApplication {
     public static void main(String[] args) {
         SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+
+    // 注解支持的配置Bean
+    @Bean
+    public SentinelResourceAspect sentinelResourceAspect() {
+        return new SentinelResourceAspect();
     }
 }
--- spring-boot-mybatisplus/src/main/java/com/ch/study/controller/ConfigInfoController.java
+++ spring-boot-mybatisplus/src/main/java/com/ch/study/controller/ConfigInfoController.java
@@ -1,8 +1,10 @@
 package com.ch.study.controller;
 
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.ch.study.model.auto.ConfigInfo;
 import com.ch.study.service.IConfigInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,7 @@ import java.util.List;
  * @author astupidcoder
  * @since 2024-01-31
  */
+@Slf4j
 @RestController
 @RequestMapping("/config-info")
 public class ConfigInfoController {
@@ -27,13 +30,38 @@ public class ConfigInfoController {
     private IConfigInfoService configInfoService;
 
     @PostMapping("/getConfigInfo")
+    @SentinelResource(value = "common-resource", blockHandler = "getConfigInfoBlock", fallback = "getConfigInfoFallback")
     public ConfigInfo getConfigInfo() {
+        log.info("getConfigInfo success");
         return configInfoService.getById(1);
     }
 
     @PostMapping("/findAllConfig")
+    @SentinelResource(value = "findAllConfig", blockHandler = "findAllConfigBlock", fallback = "findAllConfigFallback")
     public List<ConfigInfo> findAllConfig() {
+        log.info("findAllConfig success");
         return configInfoService.findAllConfig();
     }
 
+    public List<ConfigInfo> findAllConfigBlock() {
+        log.info("==findAllConfigBlock execute==");
+        return null;
+    }
+
+    public List<ConfigInfo> findAllConfigFallback() {
+        log.info("==findAllConfigFallback execute==");
+        return null;
+    }
+
+    // Block 异常处理函数，参数最后多一个 BlockException，其余与原函数一致.
+    public ConfigInfo getConfigInfoBlock() {
+        log.info("==getConfigInfoBlock execute==");
+        return new ConfigInfo();
+    }
+
+    // Fallback 函数，函数签名与原函数一致或加一个 Throwable 类型的参数.
+    public ConfigInfo getConfigInfoFallback() {
+        log.info("==getConfigInfoFallback execute==");
+        return new ConfigInfo();
+    }
 }
